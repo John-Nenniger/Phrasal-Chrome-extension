a878096706c9ef2cdc9287685411b25cdb4a44a9
John Nenniger <JPNENNIGER@GMAIL.com>
2017-04-03 09:39:26 -0700

    still working on fetch

--- indexTab.js
+++ indexTab.js
@@ -1,1 +1,38 @@
-chrome.tabs.create(object createProperties, function callback)
+const baseURL = 'http://localhost:3000/api/v1/contents';
+
+
+function Q (query) { return document.querySelector(query) }
+function Qs (query) { return document.querySelectorAll(query) }
+
+function sendhighlighted(highlighted) {
+  fetch(`${baseURL}`)
+  .then((response) => response.json())
+  .then(console.log(response))
+}
+
+function getSelectionText() {
+  var text = "";
+  if (window.getSelection) {
+    text = window.getSelection().toString();
+  } else if (document.selection && document.selection.type != "Control") {
+    text = document.selection.createRange().text;
+  }
+  return text;
+}
+
+chrome.browserAction.onClicked.addListener(function() {
+  // first it should send a request to the server with the highlighted text
+  let highlighted = getSelectionText()
+
+
+  // then it should use the response to generate the page below
+  chrome.tabs.create({selected: false ,url: chrome.extension.getURL('indexTab.html')})
+
+  sendhighlighted(highlighted)
+  console.log("Hey, whaddup?")
+
+});
+chrome.tabs.onCreated.addListener(function() {
+  console.log("please work please")
+
+})
